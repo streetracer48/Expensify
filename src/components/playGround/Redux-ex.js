c09a29f97880607d865217bd5825f286e30b1024
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-10 20:25:35 +0600

    exercise redux #3  with dispatch actions throught switch case

--- src/components/playGround/Redux-ex.js
+++ src/components/playGround/Redux-ex.js
@@ -2,16 +2,14 @@ import React from 'react'
 import {createStore} from  'redux'
 const store = createStore((state = {count:45}, action) => {
 
-    if(action.type ==='INCREMENT')
-    {
-        return {
-            count:state.count +1
-        };
+    switch(action.type) {
+       case 'INCREMENT':
+       return {
+           count: state.count +1 
+       };
+       default:
+       return state;
     }
-     else {
-
-          return state;
-     }
 });
 
 console.log(store.getState())
