e80ee33b809e7243c691fb971ab78536ba873c81
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-10 20:22:56 +0600

    exercise redux #2 with dispatch actions throught if case

--- src/components/playGround/Redux-ex.js
+++ src/components/playGround/Redux-ex.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import {createStore} from  'redux'
-const store = createStore((state = {count:45}) => {
+const store = createStore((state = {count:45}, action) => {
 
-    return state;
+    if(action.type ==='INCREMENT')
+    {
+        return {
+            count:state.count +1
+        };
+    }
+     else {
+
+          return state;
+     }
+});
+
+console.log(store.getState())
+
+//actions - than an object that gets sent to the store
+
+//increment, decrement, reset
+//walk, stop_walking, sit, work, stop_working
+//I'd like to increment the count
+store.dispatch({
+    type:'INCREMENT'
 })
+
+store.dispatch({
+    type:'INCREMENT'
+})
+
+console.log(store.getState())
+//I'd like reset the count to zero
 
 
 const Reduxtest = () => (
-    console.log(store.getState()),
+  
+
     <h1>hello this is redux test</h1>    
     
   )
