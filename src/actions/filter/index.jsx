2142c0f3009b3707ee202ef0c47036b45426116b
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-11 18:25:09 +0600

    all actions added export key it was my mistake

--- src/actions/filter/index.jsx
+++ src/actions/filter/index.jsx
@@ -1,28 +1,28 @@
 
 // SET_TEXT_FILTER
-const setTextFilter = (text = '') => ({
+export const setTextFilter = (text = '') => ({
     type: 'SET_TEXT_FILTER',
     text
   });
   
   // SORT_BY_DATE
-  const sortByDate = () => ({
+  export const sortByDate = () => ({
     type: 'SORT_BY_DATE'
   });
   
   // SORT_BY_AMOUNT
-  const sortByAmount = () => ({
+  export const sortByAmount = () => ({
     type: 'SORT_BY_AMOUNT'
   });
   
   // SET_START_DATE
-  const setStartDate = (startDate) => ({
+  export const setStartDate = (startDate) => ({
     type: 'SET_START_DATE',
     startDate
   });
   
   // SET_END_DATE
-  const setEndDate = (endDate) => ({
+  export const setEndDate = (endDate) => ({
     type: 'SET_END_DATE',
     endDate
   });
