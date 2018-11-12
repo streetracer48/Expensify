3d3e3d83f98130fbf8c23e4bd8ee988decc4f56e
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-12 22:59:47 +0600

    implement remove action on react easy way

--- src/components/ExpenseList/ExpenseListItem/index.jsx
+++ src/components/ExpenseList/ExpenseListItem/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import {removeExpense} from '../../../actions/expenses'
+import {connect} from 'react-redux'
 
-const ExpenseListItem =({description,amount, createdAt}) => (
+const ExpenseListItem =({dispatch,description,amount, id, createdAt}) => (
 
     <div>
         <h3>{description}</h3>
         <h3>{amount} - {createdAt}</h3>
+        <button onClick={()=>dispatch(removeExpense({id})) }>Remove</button>
 
     </div>
 ) 
 
-export default ExpenseListItem
+export default connect() (ExpenseListItem)
